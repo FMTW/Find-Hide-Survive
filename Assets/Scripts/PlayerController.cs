f0b084829c313f2d2d6f3ff437b55426903d4167
Norris Wu <Norris.wu.au@outlook.com>
2018-09-12 12:11:02 +1000

    Updated Ton of stuffs
    - Upload 5 PlayerControl Characters
    - Upload Map
    - Upload Hanger Section
    - Upload Prop
    - Upload Temporary Texture
    - Upload Unity 2D Extras Package
    - Upload Prefebs for range of item
    - Upload 1 scene
    - Update connection for Campaign button

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    Camera viewCamera;
 
     public float sprintSpeed = 50;
     public float walkSpeed = 20;
     
     private Rigidbody2D rb2d;
+    private Vector3 MousePos;
 
     void Start() {
-
         rb2d = GetComponent<Rigidbody2D>();
+        viewCamera = Camera.main;
 
     }
 
     void FixedUpdate() {
-
-
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -28,6 +28,11 @@ public class PlayerController : MonoBehaviour {
         faceMouse();  // Aim the game object towards the mouse position.
 
     }
+
+    void Update() {
+
+    }
+
 
     // Check if "Shift" is pressed then increase speed.
     void move(Vector2 _movement) {
@@ -40,10 +45,10 @@ public class PlayerController : MonoBehaviour {
     // Hey Purich, this should fix the issue with where the character will aim towards.
     void faceMouse()
     {
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        Vector3 MousePos = Input.mousePosition;
+        MousePos = Camera.main.ScreenToWorldPoint(MousePos);
 
-        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        Vector2 direction = new Vector2(MousePos.x - transform.position.x, MousePos.y - transform.position.y);
 
         transform.up = direction;
     }
---
+++ Assets/Scripts/WIP/FieldOfView.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour {
+
+    public float viewRadius;
+    public float viewAngle;
+
+    Collider2D[] playerInRadius;
+    [SerializeField] LayerMask obstacleMask, playerMash;
+    public List<Transform> visiblePlayer = new List<Transform>();
+
+
+    void FixedUpdate() {
+        findVisiblePlayer();
+    }
+
+    void findVisiblePlayer() {
+        playerInRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius);
+
+        for (int i = 0; i < playerInRadius.Length; i++) {
+            Transform player = playerInRadius[i].transform;
+            Vector2 dirPlayer = new Vector2(player.position.x - transform.position.x, player.position.y - transform.position.y);
+            if (Vector2.Angle(dirPlayer, transform.right) < viewAngle / 2) {
+                float distancePlayer = Vector2.Distance(transform.position, player.position);
+
+                if (!Physics2D.Raycast(transform.position, dirPlayer, distancePlayer, obstacleMask)) {
+                    visiblePlayer.Add(player);
+                }
+            }
+
+        }
+
+    }
+
+    public Vector3 DirFromAngle(float _angleInDegrees, bool global) {
+        if (!global)
+        {
+            _angleInDegrees += transform.eulerAngles.z;
+        }
+        return new Vector3(Mathf.Sin(_angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(_angleInDegrees * Mathf.Deg2Rad));
+    }
+
+	
+}
